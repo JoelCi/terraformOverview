b616dd9c16d50dd94e97e77d9e188a075dbf0d13
joe <joelsciccone@gmail.com>
2022-02-11 17:16:20 -0300

    adding approval gate for development environment

--- infra/main.tf
+++ infra/main.tf
@@ -6,21 +6,21 @@ module "myVPC" {
   public_subnets_cidr  = var.public_subnets_cidr
 
 }
-#module "publicEC2" {
-#  source        = "github.com/JoelCi/terraformOverview/modules/ec2Instances"
-#  aws_subnet_id = module.myVPC.aws_subnet_public_subnet_id
-#  numberOfEC2   = var.numberOfPublicEC2
-#  environment   = var.environment
-#  type          = "public"
-#}
-#
-#module "privateEC2" {
-#  source        = "github.com/JoelCi/terraformOverview/modules/ec2Instances"
-#  aws_subnet_id = module.myVPC.aws_subnet_private_subnet_id
-#  numberOfEC2   = var.numberOfPrivateEC2
-#  environment   = var.environment
-#  type          = "private"
-#}
-#
-#
-#
+module "publicEC2" {
+  source        = "github.com/JoelCi/terraformOverview/modules/ec2Instances"
+  aws_subnet_id = module.myVPC.aws_subnet_public_subnet_id
+  numberOfEC2   = var.numberOfPublicEC2
+  environment   = var.environment
+  type          = "public"
+}
+
+module "privateEC2" {
+  source        = "github.com/JoelCi/terraformOverview/modules/ec2Instances"
+  aws_subnet_id = module.myVPC.aws_subnet_private_subnet_id
+  numberOfEC2   = var.numberOfPrivateEC2
+  environment   = var.environment
+  type          = "private"
+}
+
+
+
